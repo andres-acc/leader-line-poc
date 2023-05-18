31ac621f185db2798834b960e39202f860787178
andres-acc <elkin.alcaraz@accenture.com>
2023-05-18 07:02:46 -0500

    Add basic example of leader line

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as LeaderLine from 'leader-line-new';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('start', { read: ElementRef }) startElement?: ElementRef;
+  @ViewChild('end', { read: ElementRef }) endElement?: ElementRef;
   title = 'leader-line-poc';
+
+  ngAfterViewInit() {
+    if(this.startElement && this.endElement) {
+      const line = new LeaderLine(this.startElement.nativeElement, this.endElement.nativeElement,
+        {
+          path: 'grid'
+        });
+    }
+  }
 }
